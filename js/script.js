8022c24eb9d0dcd4a9c7b45e0a613cf6b6b5a1a7
Oba <adelusiobafunsho@gmail.com>
2023-04-26 14:46:42 -0500

    Update homepage content and styles

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-const r = document.querySelector(":root");
 const copyEl = document.getElementById("copyright");
 
 const menuIcon = document.getElementById("menu-icon"),
@@ -10,14 +9,3 @@ copyEl.innerHTML = `&copy; ${currentYear} • Built by Oba Adelusi`;
 menuIcon.addEventListener("click", () => {
     sidebar.classList.toggle("show");
 });
-
-// Change value of primary hue
-// function getRandomHue() {
-//     const MAX_HUE = 360;
-//     return Math.floor(Math.random() * MAX_HUE);
-// }
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const rand = getRandomHue();
-//     r.style.setProperty('--hue', rand);
-// })
