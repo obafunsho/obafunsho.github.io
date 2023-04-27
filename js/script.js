33de0039131df68d8aca9f453de1e1c8071675d4
Oba <adelusiobafunsho@gmail.com>
2023-04-26 21:58:49 -0500

    Added contact form validation

--- js/script.js
+++ js/script.js
@@ -1,11 +1,115 @@
-const copyEl = document.getElementById("copyright");
-
 const menuIcon = document.getElementById("menu-icon"),
     sidebar = document.getElementById("sidebar");
-
-const currentYear = new Date().getFullYear();
-copyEl.innerHTML = `&copy; ${currentYear} • Built by Oba Adelusi`;
 
 menuIcon.addEventListener("click", () => {
     sidebar.classList.toggle("show");
 });
+
+function load() {
+    document.getElementById("reset").addEventListener("click", clearFields);
+
+    document.getElementById("contact-form").addEventListener("submit", (e) => {
+        e.preventDefault();
+        hideErrors();
+
+        if (!formHasErrors()) {
+            showModal();
+            clearFields();
+        }
+    });
+
+    hideErrors();
+}
+
+function showModal() {
+    const modal = document.getElementById("modal");
+
+    const contactName = document.getElementById("name").value;
+
+    document.getElementById("modal-wrapper").innerHTML = `<h3>Hey ${contactName} ✅,</h3> <p>Thank you for saying hi. <br> Will respond by email shortly.</p><button class="btn--black" id="close-modal">Close</button>`;
+
+    modal.classList.add("show");
+
+    const closeModalBtn = document.getElementById("close-modal");
+    closeModalBtn.addEventListener("click", () => {
+        modal.classList.remove("show");
+    });
+}
+
+function formHasErrors() {
+    let hasErrors = false;
+
+    const phoneValue = document.getElementById("phone").value;
+    const emailValue = document.getElementById("email").value;
+
+    if (formInputIsEmpty()) {
+        hasErrors = true;
+    }
+
+    const phoneRegex = new RegExp(/^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/);
+    const phoneIsValid = phoneRegex.test(trim(phoneValue));
+
+    if (trim(phoneValue).length > 0 && !phoneIsValid) {
+        document.getElementById(`phoneformat_error`).style.display = "block";
+        hasErrors = true;
+    }
+
+    const emailRegex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+    const emailIsValid = emailRegex.test(trim(emailValue));
+
+    if (trim(emailValue).length > 0 && !emailIsValid) {
+        document.getElementById(`emailformat_error`).style.display = "block";
+        hasErrors = true;
+    }
+
+    return hasErrors;
+}
+
+function formInputIsEmpty() {
+    let inputIsEmpty = false;
+
+    const nameInput = document.getElementById("name"),
+        phoneInput = document.getElementById("phone"),
+        emailInput = document.getElementById("email"),
+        messageTxt = document.getElementById("message");
+
+    const contactInputs = [messageTxt, emailInput, phoneInput, nameInput];
+
+    for (const input of contactInputs) {
+        if (trim(input.value).length == 0) {
+            document.getElementById(`${input.id}_error`).style.display = "block";
+            inputIsEmpty = true;
+            input.focus();
+        }
+    }
+
+    return inputIsEmpty;
+}
+
+function clearFields() {
+    const inputs = document.getElementsByTagName("input");
+
+    for (const input of inputs) {
+        input.value = "";
+    }
+}
+
+function hideErrors() {
+    const errors = document.getElementsByClassName("input-error");
+
+    for (const inputError of errors) {
+        inputError.style.display = "none";
+    }
+}
+
+/**
+ * Removes whitespace from a string value.
+ * @param str The string to trim.
+ * @returns A string with leading and trailing white-space removed.
+ */
+function trim(str) {
+    // Uses a regex to remove spaces from a string.
+    return str.replace(/^\s+|\s+$/g, "");
+}
+
+document.addEventListener("DOMContentLoaded", load);
